0e29c5d97e616652c757e08c334f439512768e00
Tewodiros Fikadu <tewodrosfikadu499@gmail.com>
2026-03-26 13:15:45 +0300

    your message

--- models/service_record.py
+++ models/service_record.py
@@ -2,8 +2,12 @@ from odoo import models, fields
 
 class ServiceRecord(models.Model):
     _name = 'mesob.service.record'
+    _description = 'Service Record'
 
     vehicle_id = fields.Many2one('fleet.vehicle', required=True)
     date = fields.Date()
     cost = fields.Float()
     description = fields.Text()
+    service_type = fields.Char()
+    fuel_volume = fields.Float()
+    odometer = fields.Float()
