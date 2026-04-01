2ea1e3460998cf69a617ad90cfc8f39112b8ef2b
Tewodiros Fikadu <tewodrosfikadu499@gmail.com>
2026-04-01 12:12:01 +0300

    your message

--- models/trip_log.py
+++ models/trip_log.py
@@ -2,6 +2,7 @@ from odoo import models, fields
 
 class TripLog(models.Model):
     _name = 'mesob.trip.log'
+    _description = 'Trip Odometer Log'
 
     trip_id = fields.Many2one('mesob.trip.request')
     start_odometer = fields.Float()
